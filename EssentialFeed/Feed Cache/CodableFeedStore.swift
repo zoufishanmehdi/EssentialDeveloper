9638c7872c288469ad0c0bfc0a1fe91cce4d7dd3
zoufishanmehdi <zzmehdi@gmail.com>
2023-07-02 12:28:45 -0400

    Create FeedStoreSpecs that must be implemented by any FeedStore test implementation to guarantee its meet spec

---
+++ EssentialFeed/Feed Cache/CodableFeedStore.swift
@@ -0,0 +1,8 @@
+//
+//  CodableFeedStore.swift
+//  EssentialFeed
+//
+//  Created by Zouf on 7/2/23.
+//
+
+import Foundation
---
+++ EssentialFeedTests/Feed Cache/FeedStoreSpecs.swift
@@ -0,0 +1,8 @@
+//
+//  FeedStoreSpecs.swift
+//  EssentialFeedTests
+//
+//  Created by Zouf on 7/2/23.
+//
+
+import Foundation
